cd15b0178f937c4ac4209938ace804e78c97c049
slavomirus <krisjanik5@gmail.com>
2025-10-28 23:10:19 +0100

    our first project hihi

---
+++ main.py
@@ -0,0 +1,63 @@
+from queue import PriorityQueue
+
+message = "Hello World Witaj świecie Ciao Mondo"
+# print(message)
+# for i in range(10):
+#     print(f'{i}', message)
+#
+# i = 0
+
+# while i != 100:
+#     print(f'{i}', message)
+#     i+=1 # i = i + 1 ===== i+=1
+
+
+a = 6 # integer
+b = 10 # number ten
+c = a + b # addition
+d = b - a # subtraction
+e = a * b # multiplication
+f = b / a # division 3.3333
+g = b % a # modulo
+h = a ** b # exponent
+j = b // a # floor division 3
+
+print(a, b, c, d, e, f, g, h, j)
+
+
+word1 = "Samu "
+wyraz2 = "loves "
+wyraz3 = "Martyna "
+word4 = "hates "
+wrod5 = "pineapple on pizza"
+
+sentence = word1 + wyraz2 + wyraz3
+print(sentence)
+
+sentence2 = word1 + word4 + wrod5
+print(sentence2)
+
+print(sentence * 4)
+
+x = 20.1
+y = 20
+
+# == -> is equal to
+# != -> is not equal to
+
+# if x != y:
+#     if x < y:
+#         # when x is smaller than y
+#         x+=1
+#         print(x, y)
+#     elif x > y:
+#         # when x is larger than y
+#         x-=1
+#         print(x, y)
+#     # when they are not the same no matter larger/smaler
+#     print("they are not equal!")
+#     print(x, y)
+# else: # when they is the same
+#     print("they are equal!")
+#     print(x, y)
+
